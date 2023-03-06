e96ce81cd8140c7f727408c1323992d3980ffc11
nandacruz <fernanda.moura.cruz98@gmail.com>
2023-03-06 14:03:22 -0300

    estilizando listagem de grupo de usuario

--- resources/views/sistema/usuarioGrupo/listar/view.blade.php
+++ resources/views/sistema/usuarioGrupo/listar/view.blade.php
@@ -1,11 +1,47 @@
-<table>
-@foreach($dataGpUsuarioDao as $vUsuaGpDao)
-    <tr>
-        <td>{{$vUsuaGpDao->getId()}}</td>
-        <td>{{$vUsuaGpDao->getUuid()}}</td>
-        <td>{{$vUsuaGpDao->getName()}}</td>
-        <td>{{$vUsuaGpDao->getDesc()}}</td>
-    </tr>
-@endforeach
-</table>
-<hr>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+      
+      <title>Listagem teste</title>
+</head>
+<body>
+      <main class="table">
+            <section class="table_header">
+                  <h1> Listagem Teste</h1>
+            </section>
+            <section class="table_body">
+                  <table>
+                        <!---Cabeçalho-->
+                        <thead>
+                              <!---Linhas do cabeçalho-->
+                              <tr>  
+                                    <!---Células do cabeçalho-->
+                                    <th>Id</th>
+                                    <th>uuid</th>
+                                    <th>name</th>
+                                    <th>description</th>  
+                              </tr>
+
+                        </thead>
+                        <!---Corpo da tabela-->
+                        <tbody>
+                        @foreach($dataGpUsuarioDao as $vUsuaGpDao)
+                            <tr>
+                                <td>{{$vUsuaGpDao->getId()}}</td>
+                                <td>{{$vUsuaGpDao->getUuid()}}</td>
+                                <td>{{$vUsuaGpDao->getName()}}</td>
+                                <td>{{$vUsuaGpDao->getDesc()}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                        
+                  </table>
+
+            </section>
+      </main>
+</body>
+</html>
